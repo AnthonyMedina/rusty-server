c005321bcd34c559f9fe126f8951218dc8b0c14d
Anthony Medina <antmedina91@gmail.com>
2019-02-10 22:52:39 +0000

    serve 404 on non GET methods

--- src/main.rs
+++ src/main.rs
@@ -29,5 +29,12 @@ fn handle_connection(mut stream: TcpStream) {
 
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
+    } else {
+        let contents = fs::read_to_string("404.html").unwrap();
+
+        let response = format!("HTTP/1.1 404 NOT FOUND\r\n\r\n{}", contents);
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
     }
 }
